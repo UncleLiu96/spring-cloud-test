44c70951f20cd8139ae9fd0c6aab71505876d340
Uncle-LiuY <nanfonjin@foxmail.com>
2019-08-10 14:23:58 +0800

    1.0

--- common/src/main/java/com/common/CommonApplication.java
+++ common/src/main/java/com/common/CommonApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(CommonApplication.class, args);
     }
 
