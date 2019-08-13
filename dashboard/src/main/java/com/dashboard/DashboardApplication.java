efeaf030ac6e5922bfe2ab9f321c9a5d2855e2e6
Uncle-LiuY <nanfonjin@foxmail.com>
2019-08-13 17:20:48 +0800

    仪表盘

--- dashboard/src/main/java/com/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/dashboard/DashboardApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 public class DashboardApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DashboardApplication.class, args);
     }
 
