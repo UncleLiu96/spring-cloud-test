5dea63b8bfc0db90fe8764f57b046319b8f2606a
Uncle-LiuY <nanfonjin@foxmail.com>
2019-08-26 13:45:40 +0800

    config

--- api/src/main/java/com/api/demo/controller/TestController.java
+++ api/src/main/java/com/api/demo/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.api.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class TestController {
 
     @Value("${env}")
